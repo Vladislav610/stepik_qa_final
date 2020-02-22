54eb5a97927adac5a58d8b11aa2b35a1c8ed7bea
Vladislav610 <vladislaveremeev610@yandex.ru>
2020-02-22 10:16:38 +0400

    added new check

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .Pages.main_page import MainPage
 
 
@@ -7,10 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-=======
-def test_guest_can_go_to_login_page(browser):
+
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
->>>>>>> 2d6336f0f759f7219e89344c0593c3b376d936a0
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
