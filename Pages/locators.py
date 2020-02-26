882d2fee850c88469fa502e71670e3d93c3ca0de
Vladislav610 <vladislaveremeev610@yandex.ru>
2020-02-26 10:47:18 +0400

    refactor main_page -> base_page

--- Pages/locators.py
+++ Pages/locators.py
@@ -1,14 +1,14 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-
 
 class ProductPageLocators():
     BUTTON_ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
