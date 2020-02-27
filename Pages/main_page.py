991ee40c67b079139c3bc98034c33cc48bc236b6
Vladislav610 <vladislaveremeev610@yandex.ru>
2020-02-27 19:40:28 +0400

    final fixes

--- Pages/main_page.py
+++ Pages/main_page.py
@@ -1,11 +1,8 @@
 from .base_page import BasePage
-from .locators import MainPageLocators, BasketPageLocators
 
 
 class MainPage(BasePage):
-    def should_be_go_to_basket(self):
-        button_go_to_basket = self.browser.find_element(*MainPageLocators.BUTTON_GO_TO_BASKET)
-        button_go_to_basket.click()
+    pass
 
 
 
